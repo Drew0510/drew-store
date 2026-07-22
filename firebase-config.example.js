9643017b707bad4e00592a90faefe9cfc86c6b18
Drew <dungdepzaii0510@gmail.com>
2026-07-22 10:37:16 +0700

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// Đổi tên file này thành firebase-config.js sau khi tạo Firebase project.
+// KHÔNG đặt ZaloPay key1/key2 hoặc tài khoản Minecraft trong file frontend.
+window.DREW_STORE_FIREBASE_CONFIG = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
